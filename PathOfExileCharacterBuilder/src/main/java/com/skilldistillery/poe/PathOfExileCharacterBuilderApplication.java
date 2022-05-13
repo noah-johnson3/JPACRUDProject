92a0d1f659e2a46346f545812915284c809f4f12
noah-johnson3 <noah.johnson3@yahoo.com>
2022-05-13 18:50:26 -0400

    created boot mvc project, added JPA project dependency

---
+++ PathOfExileCharacterBuilder/src/main/java/com/skilldistillery/poe/PathOfExileCharacterBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.poe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathOfExileCharacterBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathOfExileCharacterBuilderApplication.class, args);
+	}
+
+}
